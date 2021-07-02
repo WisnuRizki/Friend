e59fd83e709e6cbf930dff1528385bcdd6b3799c
wisnu <akunpopo2@gmail.com>
2021-07-02 18:59:48 +0700

    Updates

---
+++ precache-manifest.138fe81dc19cfa5d1ec7e609dccff8cb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47c755c0451db2fcb44f5ccdfef0fe95",
+    "url": "/Friend/index.html"
+  },
+  {
+    "revision": "b42dac28ee2169757673",
+    "url": "/Friend/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "61d713c3c7ab7660903b",
+    "url": "/Friend/static/css/main.200429f4.chunk.css"
+  },
+  {
+    "revision": "b42dac28ee2169757673",
+    "url": "/Friend/static/js/2.601b451f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Friend/static/js/2.601b451f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61d713c3c7ab7660903b",
+    "url": "/Friend/static/js/main.f51fe7d4.chunk.js"
+  },
+  {
+    "revision": "055bb49e29f61408c141",
+    "url": "/Friend/static/js/runtime-main.f53b17ff.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Friend/static/media/SEGA.12a3db8b.woff"
+  }
+]);
